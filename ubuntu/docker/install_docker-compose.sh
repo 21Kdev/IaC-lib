526e6dc84d5a5aa434b3ed655181280a81cd2442
21Kdev <pch14545@gmail.com>
2023-12-26 21:44:44 +0900

    docker compose

---
+++ ubuntu/docker/install_docker-compose.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 권한이 필요한 작업을 수행하기 전에 권한 확인
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
