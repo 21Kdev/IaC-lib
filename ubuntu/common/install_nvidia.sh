73cdd8dce4c9ccba1186498481927614db978eb7
21Kdev <pch14545@gmail.com>
2023-12-26 21:40:49 +0900

    nvidia driver

---
+++ ubuntu/common/install_nvidia.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# 권한이 필요한 작업을 수행하기 전에 권한 확인
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+ubuntu-drivers install nvidia:525-server
+
+# install nvidia-container-toolkit
+apt update && apt-get install -y ca-certificates \
+    curl \
+    software-properties-common \
+    apt-transport-https \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+apt-get update && apt-get install -y nvidia-container-toolkit
