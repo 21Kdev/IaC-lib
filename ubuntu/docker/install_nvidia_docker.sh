5f444535aefb31ffdbd48da457b76d1915d2507b
21Kdev <pch14545@gmail.com>
2023-12-26 21:44:22 +0900

    nvidia docker

---
+++ ubuntu/docker/install_nvidia_docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 권한이 필요한 작업을 수행하기 전에 권한 확인
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# install nvidia-docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update && apt-get install -y nvidia-docker2 nvidia-container-runtime
+systemctl restart docker
+
+# set default runtimes as nvidia
+cat <<EOF > /etc/docker/daemon.json
+{
+    "default-runtime": "nvidia",
+    "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
+systemctl restart docker
---
+++ ubuntu/docker/install_nvidia_docker_compose_package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 권한이 필요한 작업을 수행하기 전에 권한 확인
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+echo "Installing NVIDIA driver..."
+./common/install_nvidia.sh
+
+echo "Installing Docker..."
+./install_docker.sh
+
+echo "Adding Docker permissions..."
+./add_docker_permission.sh
+
+echo "Installing NVIDIA Docker..."
+./install_nvidia_docker.sh
+
+echo "Installing Docker Compose..."
+./isntall_docker-compose.sh
+
+echo "All installations complete!"
