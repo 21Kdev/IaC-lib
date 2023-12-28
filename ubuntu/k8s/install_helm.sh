26e9d0aa2dfe9e0ff078802b09b28fef9bdd50ef
21Kdev <pch14545@gmail.com>
2023-12-28 15:16:40 +0900

    install helm

--- ubuntu/k8s/install_helm.sh
+++ ubuntu/k8s/install_helm.sh
@@ -3,4 +3,6 @@
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-# rm ./get_helm.sh
+rm ./get_helm.sh
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
