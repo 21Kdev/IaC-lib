4a0b9723ef49a69e310b9df24dc282bb108b1c2a
21Kdev <pch14545@gmail.com>
2023-12-28 16:15:17 +0900

    add package k8s

---
+++ ubuntu/k8s/package_k8s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 권한이 필요한 작업을 수행하기 전에 권한 확인
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+echo "Installing k8s..."
+sh ./install_k8s.sh
+
+echo "Installing helm..."
+sh ./install_helm.sh
+
+echo "All installations complete!"
