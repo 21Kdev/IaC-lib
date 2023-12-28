0239be5af12915eb1037496e8e01d9ecda96da34
21Kdev <pch14545@gmail.com>
2023-12-28 16:26:28 +0900

    update scripts

--- ubuntu/k8s/install_helm.sh
+++ ubuntu/k8s/install_helm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
--- ubuntu/k8s/package_nvidia_containerd_k8s.sh
+++ ubuntu/k8s/package_nvidia_containerd_k8s.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # 권한이 필요한 작업을 수행하기 전에 권한 확인
 if [ "$(id -u)" -ne 0 ]; then
@@ -6,14 +6,14 @@ if [ "$(id -u)" -ne 0 ]; then
   exit
 fi
 
-echo "Installing NVIDIA driver..."
-sh ../common/install_nvidia.sh
-
 echo "Installing k8s..."
 sh ./install_k8s.sh
 
 echo "Installing helm..."
 sh ./install_helm.sh
+
+echo "Installing NVIDIA driver..."
+sh ../common/install_nvidia.sh
 
 echo "Installing containerd_nvidia..."
 sh ./containerd_nvidia.sh
