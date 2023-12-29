3554add688340a8b17339b362b11d3ae469f7c34
21Kdev <pch14545@gmail.com>
2023-12-30 01:13:07 +0900

    update base script

--- ubuntu/common/base.sh
+++ ubuntu/common/base.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt update && sudo apt install -y git vim net-tools
+sudo apt update && sudo apt install -y openssh-server git vim net-tools
 
 # curl -sSL https://raw.githubusercontent.com/21Kdev/IaC-lib/main/ubuntu/common/base.sh | bash
