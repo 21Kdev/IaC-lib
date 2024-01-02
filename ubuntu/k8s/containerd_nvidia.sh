79e65e1abec0afd4ae2720120a0a9d7dc94e7af1
21Kdev <pch14545@gmail.com>
2024-01-02 16:35:31 +0900

    update containerd_nvidia

--- ubuntu/k8s/containerd_nvidia.sh
+++ ubuntu/k8s/containerd_nvidia.sh
@@ -6,6 +6,15 @@ if [ "$(id -u)" -ne 0 ]; then
   exit
 fi
 
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+apt-get update && apt-get install -y nvidia-container-toolkit
+
 # update containerd config
 cat > /etc/containerd/config.toml << EOF
 # For OpenSSH CoreDNS
