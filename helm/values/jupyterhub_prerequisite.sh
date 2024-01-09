372d26577051ad90a0c466eb520bfb435d7f5e9d
21Kdev <pch14545@gmail.com>
2024-01-09 15:19:33 +0900

    update jh.yaml

--- helm/values/jupyterhub_prerequisite.sh
+++ helm/values/jupyterhub_prerequisite.sh
@@ -2,5 +2,3 @@
 
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.20/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path  -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-
-sudo mkdir -p /opt/bitnami/jupyterhub-singleuser
