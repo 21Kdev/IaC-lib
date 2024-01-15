12f896ab4cb37dff20f67dba91c15837fcc7363f
21Kdev <pch14545@gmail.com>
2024-01-15 10:21:44 +0900

    add jupyter

--- ubuntu/common/base.sh
+++ ubuntu/common/base.sh
@@ -2,3 +2,4 @@
 sudo apt update && sudo apt install -y openssh-server git vim net-tools
 
 # curl -sSL https://raw.githubusercontent.com/21Kdev/IaC-lib/main/ubuntu/common/base.sh | bash
+# git clone https://github.com/21Kdev/IaC-lib.git
